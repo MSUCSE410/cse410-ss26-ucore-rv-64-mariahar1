41f0dfe5f429acd3beade6a379864f120b02e864
Harshitha Maria Rock <143450416+mariahar1@users.noreply.github.com>
2026-02-05 18:43:43 +0000

    Chapter 3 proj 1 Submission

--- os/proc.h
+++ os/proc.h
@@ -4,6 +4,7 @@
 #include "types.h"
 
 #define NPROC (16)
+#define MAX_SYSCALL_NUM 500
 
 // Saved registers for kernel context switches.
 struct context {
@@ -27,6 +28,23 @@ struct context {
 
 enum procstate { UNUSED, USED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
+/*
+* LAB1: you may need to define struct for TaskInfo here
+*/
+typedef enum {
+    UnInit,
+    Ready,
+    Running,
+    Exited,
+} TaskStatus;
+
+
+struct TaskInfo {
+	TaskStatus status;
+	unsigned int syscall_times[MAX_SYSCALL_NUM];
+	int time;
+};
+
 // Per-process state
 struct proc {
 	enum procstate state; // Process state
@@ -38,11 +56,9 @@ struct proc {
 	/*
 	* LAB1: you may need to add some new fields here
 	*/
+	unsigned int syscall_times[MAX_SYSCALL_NUM];
+    uint64 start_time;
 };
-
-/*
-* LAB1: you may need to define struct for TaskInfo here
-*/
 
 struct proc *curr_proc();
 void exit(int);
--- os/syscall.c
+++ os/syscall.c
@@ -39,6 +39,21 @@ uint64 sys_gettimeofday(TimeVal *val, int _tz)
 /*
 * LAB1: you may need to define sys_task_info here
 */
+uint64 sys_task_info(struct TaskInfo *ti) {
+    struct proc *p = curr_proc();
+
+	//running
+    ti->status = 2;
+
+	// getting syscall tume from kernal
+    for (int i = 0; i < MAX_SYSCALL_NUM; i++) {
+        ti->syscall_times[i] = p->syscall_times[i];
+    }
+
+    ti->time = (int)((get_cycle() - p->start_time) / (CPU_FREQ / 1000)); // time calc
+
+    return 0;
+}
 
 extern char trap_page[];
 
@@ -53,6 +68,9 @@ void syscall()
 	/*
 	* LAB1: you may need to update syscall counter for task info here
 	*/
+	if (id >= 0 && id < MAX_SYSCALL_NUM) {
+    	curr_proc()->syscall_times[id]++;
+	}
 	switch (id) {
 	case SYS_write:
 		ret = sys_write(args[0], (char *)args[1], args[2]);
@@ -69,6 +87,10 @@ void syscall()
 	/*
 	* LAB1: you may need to add SYS_taskinfo case here
 	*/
+    case SYS_task_info:
+        ret = sys_task_info((struct TaskInfo *)args[0]);
+        break;
+
 	default:
 		ret = -1;
 		errorf("unknown syscall %d", id);
